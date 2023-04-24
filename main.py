cca1adf2fef31914c1e739e4e759e1ab1545b1cf
Evelina Stonyte <stonyteevelina@gmail.com>
2023-04-24 15:10:58 +0300

    Laikas

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
-import random
+from datetime import datetime
 import calendar
 
-# def atsitiktinai(a, b):
-#     numbers = []
-#     for i in range(0, 10):
-#         number = random.randint(a, b)
-#         numbers.append(number)
-#     numbers = sorted(numbers)
-#     return numbers
-# kauliukas = atsitiktinai(1, 100)
-# print(kauliukas)
-import random 
 
-def kauliukas():
-    metimai = []
-    for i in range(0, 3):
-        metimas = random.randint(1, 6)
-        metimai.append(metimas)
-    if 5 in metimai:
-        print('Pralaimėjai')
-    else:
-        print('Laimėjai')
+# now_date = datetime.now()
+# format_string = '%M:%S'
 
-kauliukas()
+# date_string = now_date.strftime(format_string)
+# print(now_date)
 
-def calendor_dates(year, month):
-    print(calendar.month(year, month))
-    month_days = calendar.monthrange(year, month)[1]
-    weekdays = 0
-    for day in range(1, month_days + 1): 
-        weekday = calendar.weekday(year, month, day)
-        if weekday == 5 or weekday == 6:
-            weekdays += 1
-    print(f'Weekdays in this month: {weekdays}')
+# def age(birthday):
+#     now_date = datetime.now()
+#     format_string = '%Y-%m-%d'
+#     birthday_string = datetime.strptime(birthday, format_string)
+#     birthday = birthday_string.replace(year=now_date.year)
+    
+#     if birthday < now_date:
+#         birthday = birthday.replace(year=now_date.year + 1)
+    
+#     until_birthday = birthday_string - now_date
+#     print(f'Iki gimtadienio liko {until_birthday.days} dienų.')
+    
 
-year = int(input('Write a year: '))
-month = int(input('Write a month: '))
-calendor_dates(year, month)
 
-print('Sveiki')
-print('Hey')
+# birthday = input('Write your birthday: ')
+# age(birthday)
+
+# date = str(input('Wriet a date: '))
+# format_string = '%Y-%m-%d %M:%S'
+# date = datetime.strptime(date, format_string)
+# new_date = date - timedelta(hours=48)
+# new_date = datetime.strftime(new_date, format_string)
+# print(new_date)
+
+
+# def difference(timestamp1, timestamp2):
+#     date_object1 = datetime.fromtimestamp(timestamp1)
+#     date_object2 = datetime.fromtimestamp(timestamp2)
+#     answer = abs(timestamp1 - timestamp2)
+#     print(answer) 
+
+# timestamp1 = 1671395400
+# timestamp2 = 1674000000
+# difference(timestamp1, timestamp2)
+
+def what_day(date):
+    date = datetime.strptime(date, '%Y-%m-%d')
+    days = ['Monday', 'Tuesday', 'Wensday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    print(dir(date.weekday()))
+    return days[date.weekday()]
+
+print(what_day('2023-05-19'))
+
